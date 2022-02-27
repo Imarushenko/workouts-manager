76d0bef2799f568db8619f6097e6ddd413623a71
Igor Marushenko <igormarushenko@gmail.com>
2022-02-27 16:24:30 +0200

    create login & register functions and check it for usabillity in Firebase - works!

--- app/src/main/java/com/example/workout_manager/AppRegister.java
+++ app/src/main/java/com/example/workout_manager/AppRegister.java
@@ -1,14 +1,45 @@
 package com.example.workout_manager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AppRegister extends AppCompatActivity {
+    // firebase
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_register);
+        // firebase
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    // register function
+    public void register(View view) {
+        String email = ((EditText) findViewById(R.id.email_field_reg)).getText().toString();
+        String password = ((EditText) findViewById(R.id.pass_field_reg)).getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("result" , "registry - OK");
+                        } else {
+                            Log.d("result" , "registry - Failed");
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/workout_manager/MainActivity.java
+++ app/src/main/java/com/example/workout_manager/MainActivity.java
@@ -1,18 +1,49 @@
 package com.example.workout_manager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+    // firebase
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // firebase
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    // login function
+    public void login(View view) {
+        // get email address & password
+        String email = ((EditText)findViewById(R.id.field_email)).getText().toString();
+        String password = ((EditText)findViewById(R.id.field_password)).getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("result" , "login OK");
+                        } else {
+                            Log.d("result" , "login FAILED");
+                        }
+                    }
+                });
     }
 
     public void checkValidation(View view) {
