7fe05b7d77578331b5c95089fe15d2c3baec0ad1
Igor Marushenko <igormarushenko@gmail.com>
2022-02-28 12:09:01 +0200

    add user file

---
+++ app/src/main/java/com/example/workout_manager/User.java
@@ -0,0 +1,15 @@
+package com.example.workout_manager;
+
+public class User {
+    String name;
+    String id;
+    String phone;
+    String address;
+
+    public User(String name, String id, String phone, String address) {
+        this.name = name;
+        this.id = id;
+        this.phone = phone;
+        this.address = address;
+    }
+}
