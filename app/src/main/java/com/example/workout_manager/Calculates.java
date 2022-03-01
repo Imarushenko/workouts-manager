7cc477db2e3b84eba8127a6a880dc041a9a2f888
Igor Marushenko <igormarushenko@gmail.com>
2022-03-01 16:25:38 +0200

    create calculates class and activity. start working on calculations

---
+++ app/src/main/java/com/example/workout_manager/Calculates.java
@@ -0,0 +1,14 @@
+package com.example.workout_manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calculates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculates);
+    }
+}
