fce249a26834cdaa14c7ff801c690a33f7dd3ba4
Igor Marushenko <igormarushenko@gmail.com>
2022-03-02 15:59:40 +0200

    Async Task is working - need to improve the functuionallity

--- app/src/main/java/com/example/workout_manager/Calculates.java
+++ app/src/main/java/com/example/workout_manager/Calculates.java
@@ -1,9 +1,12 @@
 package com.example.workout_manager;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,11 +21,13 @@ public class Calculates extends AppCompatActivity {
     EditText heightBMR;
     EditText weightBMR;
     TextView resultBMR;
+    Button BMR_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculates);
+        String res = "";
         // get fields by id from the activity
         // BMI
         height = (EditText) findViewById(R.id.weight_field);
@@ -33,6 +38,14 @@ public class Calculates extends AppCompatActivity {
         heightBMR = (EditText) findViewById(R.id.height_bmr_field);
         weightBMR = (EditText) findViewById(R.id.weight_bmr_field);
         resultBMR = (TextView) findViewById(R.id.result_bmr);
+        BMR_btn = (Button) findViewById(R.id.calculate_BMR_btn);
+        // click button " calculate bmr "
+        BMR_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new CalculateBmiBmr().execute();
+            }
+        });
     }
 
     public void calculateBMI(View v) {
@@ -47,16 +60,41 @@ public class Calculates extends AppCompatActivity {
 
     }
 
-    public void calculateBMR(View v) {
-        String ageStr = age.getText().toString();
-        String hStr = heightBMR.getText().toString();
-        String wStr = weightBMR.getText().toString();
+//    public void calculateBMR(View v) {
+//
+//    }
 
-        float ageValue = Float.parseFloat(ageStr);
-        float hValue = Float.parseFloat(hStr) / 100;
-        float wValue = Float.parseFloat(wStr);
+    // inner class of AsyncTask
+    public class CalculateBmiBmr extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
 
-        float bmr = (float) (((ageValue * 5) - (hValue * 6.25) + (wValue * 10)) * 1.5);
-        resultBMR.setText((Float.toString(bmr)));
+        @Override
+        protected void onPostExecute(String s) {
+//            super.onPostExecute(s);
+            Toast.makeText(Calculates.this, "BMR calculated successfully!!", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                String ageStr = age.getText().toString();
+                String hStr = heightBMR.getText().toString();
+                String wStr = weightBMR.getText().toString();
+
+                float ageValue = Float.parseFloat(ageStr);
+                float hValue = Float.parseFloat(hStr) / 100;
+                float wValue = Float.parseFloat(wStr);
+
+                float bmr = (float) (((ageValue * 5) - (hValue * 6.25) + (wValue * 10)) * 1.5);
+                resultBMR.setText((Float.toString(bmr)));
+            }
+            catch (Exception e) {
+                System.out.println(e);
+            }
+            return null;
+        }
     }
 }
